5885987295628c838159f9f989f5d84802e605fe
Austin_Curran <a.curran1192@gmail.com>
2023-10-18 12:40:23 -0600

    updated about icons

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -8,21 +8,21 @@ const Info = () => {
 				<i className="bx bx-award about__icon"></i>
 
 				<h3 className="about__title">Experience</h3>
-				<span className="about__subtitle">2 Years Working</span>
+				<span className="about__subtitle">3 Years Coding</span>
 			</div>
 
 			<div className="about__box">
-				<i className="bx bx-briefcase-alt about__icon"></i>
+				<i class="bx bx-code-alt about__icon"></i>
 
 				<h3 className="about__title">Completed</h3>
-				<span className="about__subtitle">11 + Projects</span>
+				<span className="about__subtitle">15 + Projects</span>
 			</div>
 
 			<div className="about__box">
-				<i className="bx bx-support about__icon"></i>
+				<i class="bx bx-hard-hat about__icon"></i>
 
-				<h3 className="about__title">Support</h3>
-				<span className="about__subtitle">Online 24/7</span>
+				<h3 className="about__title">Background</h3>
+				<span className="about__subtitle">Non-traditional</span>
 			</div>
 		</div>
 	);
